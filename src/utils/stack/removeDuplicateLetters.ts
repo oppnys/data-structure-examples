a606987f6b870f939aa92e27db447fc440b8d733
dongtian <liu6xd@gmail.com>
2022-04-21 19:57:03 +0800

    fixed: format code

--- src/utils/stack/removeDuplicateLetters.ts
+++ src/utils/stack/removeDuplicateLetters.ts
@@ -2,17 +2,16 @@ export default function removeDuplicateLetters(s: string): string {
   let ret = '';
   for (let i = 0, len = s.length; i < len; i++) {
     if (ret && ret.includes(s[i])) {
-      debugger
       const retArr = ret.split('');
-      const cIndex = retArr.indexOf(s[i])
-      retArr.splice(cIndex, 1)
-      retArr.push(s[i])
-      const c = retArr.join('')
+      const cIndex = retArr.indexOf(s[i]);
+      retArr.splice(cIndex, 1);
+      retArr.push(s[i]);
+      const c = retArr.join('');
       if (c < ret) {
-        ret = c
+        ret = c;
       }
     } else {
-      ret += s[i]
+      ret += s[i];
     }
   }
   return ret;
--- src/utils/stack/simplifyPath.ts
+++ src/utils/stack/simplifyPath.ts
@@ -1,12 +1,12 @@
 export default function simplifyPath(path: string): string {
-    const pathes: string[] = path.split('/');
-    const ret: string[] = []
-    for (const p of pathes) {
-        if (p === '..') {
-            if (ret.length) ret.pop()
-        } else if (p.length && p !== '.') {
-            ret.push(p)
-        }
+  const pathes: string[] = path.split('/');
+  const ret: string[] = [];
+  for (const p of pathes) {
+    if (p === '..') {
+      if (ret.length) ret.pop();
+    } else if (p.length && p !== '.') {
+      ret.push(p);
     }
-    return '/' + ret.join('/')
-};
+  }
+  return `/${ret.join('/')}`;
+}
